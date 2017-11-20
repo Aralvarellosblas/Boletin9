ad95ba0d47d73e8ac756946c6aa988f8e33a16b1
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-20 08:44:53 +0100

    Programa Terminado

---
+++ Ejercicio9_4/src/ejercicio9_4/Ejercicio9_4.java
@@ -0,0 +1,13 @@
+package ejercicio9_4;
+
+/*
+ * @author Arturo
+ */
+public class Ejercicio9_4 {
+
+    public static void main(String[] args) {
+        Tabla t1=new Tabla();
+        t1.tablaMul();
+    }
+
+}
