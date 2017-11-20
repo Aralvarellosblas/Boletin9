f3ac426bbd8535a0a28de805efd3bffca7a7adea
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-20 08:45:26 +0100

    Programa Terminado

---
+++ Ejercicio9_5/src/ejercicio9_5/Ejercicio9_5.java
@@ -0,0 +1,12 @@
+package ejercicio9_5;
+/*
+ * @author Arturo
+ */
+public class Ejercicio9_5 {
+
+    public static void main(String[] args) {
+        Repeticion rep=new Repeticion();
+        rep.secuencia();
+    }
+
+}
