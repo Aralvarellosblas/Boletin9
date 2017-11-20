3ea7311e4d9fe5c288301074fd18ab563c06e875
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-20 08:43:21 +0100

    Programa Terminado

---
+++ Ejercicio9_2/src/ejercicio9_2/Ejercicio9_2.java
@@ -0,0 +1,13 @@
+package ejercicio9_2;
+/*
+ * @author Arturo
+ */
+public class Ejercicio9_2 {
+
+    public static void main(String[] args) {
+        Calcular c1=new Calcular();
+        c1.calcSum();
+        c1.calcProd();
+    }
+
+}
