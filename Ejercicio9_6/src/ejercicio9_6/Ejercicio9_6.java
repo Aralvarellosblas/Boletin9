d7fa69fc812c143718fcc3b455e72d50abe21239
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-20 08:46:19 +0100

    Programa Terminado

---
+++ Ejercicio9_6/src/ejercicio9_6/Ejercicio9_6.java
@@ -0,0 +1,12 @@
+package ejercicio9_6;
+/*
+ * @author Arturo
+ */
+public class Ejercicio9_6 {
+
+    public static void main(String[] args) {
+        Sueldos s1=new Sueldos();
+        s1.comparar();
+    }
+
+}
