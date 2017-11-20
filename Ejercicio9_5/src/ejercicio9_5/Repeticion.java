6c49195acacb797a2d1349823dd5a5674a3620a9
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-20 08:46:03 +0100

    Clase Terminada

---
+++ Ejercicio9_5/src/ejercicio9_5/Repeticion.java
@@ -0,0 +1,52 @@
+package ejercicio9_5;
+
+import java.util.Scanner;
+
+/*
+ * @author Arturo
+ */
+public class Repeticion {
+    private int num, n, i=0, x, y, z;
+    Scanner sc=new Scanner(System.in);
+    
+    public Repeticion(){
+        
+    }
+    public void secuencia(){
+        System.out.println("Selecciona una secuencia\n1- (2 4 6 8 10...)\n2- (- 1 + 2 – 3 + 4 – 5 + 6...)\n3- Fibonacci");
+        n=sc.nextInt();
+        switch(n){
+            case 1: System.out.println("Seleciona el numero de elementos de la secuencia");
+                x=0;
+                y=sc.nextInt();
+                System.out.println("Inicio secuencia");
+                while(i<y){
+                    num=x+2;
+                    i++;
+                    x=x+2;
+                    System.out.print(num+" ");
+                }
+                System.out.println();
+            break;
+            case 2: System.out.println("Seleciona el numero de elementos de la secuencia");
+                x=1;
+                z=1;
+                y=sc.nextInt();
+                System.out.println("Inicio secuencia");
+                do{
+                    num=x;
+                    i++;
+                    x++;
+                    if(num%2==0)System.out.print("+"+num+" ");
+                    else System.out.print("-"+num+" ");
+                }while(i<y);
+                System.out.println();
+            break;
+            case 3: Fibonacci f=new Fibonacci();
+                    f.serieFib();
+            break;
+            default: System.out.println("Seleccion incorrecta");
+        }
+        
+    }
+}
