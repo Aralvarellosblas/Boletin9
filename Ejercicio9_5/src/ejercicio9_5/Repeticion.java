74195a410fde2f86267426a401e3fa4316bec30b
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-23 09:51:04 +0100

    Ligeras modificaciones

--- Ejercicio9_5/src/ejercicio9_5/Repeticion.java
+++ Ejercicio9_5/src/ejercicio9_5/Repeticion.java
@@ -5,30 +5,34 @@ import java.util.Scanner;
 /*
  * @author Arturo
  */
-public class Repeticion {
-    private int num, n, i=0, x, y, z;
+public class Repeticion{
+
+    private int num, selec, i=0, x, y, z;
     Scanner sc=new Scanner(System.in);
-    
+
     public Repeticion(){
-        
+
     }
+
     public void secuencia(){
         System.out.println("Selecciona una secuencia\n1- (2 4 6 8 10...)\n2- (- 1 + 2 – 3 + 4 – 5 + 6...)\n3- Fibonacci");
-        n=sc.nextInt();
-        switch(n){
-            case 1: System.out.println("Seleciona el numero de elementos de la secuencia");
+        selec=sc.nextInt();
+        switch(selec){
+            case 1:
+                System.out.println("Seleciona el numero de elementos de la secuencia");
                 x=0;
                 y=sc.nextInt();
                 System.out.println("Inicio secuencia");
-                while(i<y){
-                    num=x+2;
+                while(i < y){
+                    num=x + 2;
                     i++;
-                    x=x+2;
-                    System.out.print(num+" ");
+                    x=x + 2;
+                    System.out.print(num + " ");
                 }
                 System.out.println();
-            break;
-            case 2: System.out.println("Seleciona el numero de elementos de la secuencia");
+                break;
+            case 2:
+                System.out.println("Seleciona el numero de elementos de la secuencia");
                 x=1;
                 z=1;
                 y=sc.nextInt();
@@ -37,16 +41,22 @@ public class Repeticion {
                     num=x;
                     i++;
                     x++;
-                    if(num%2==0)System.out.print("+"+num+" ");
-                    else System.out.print("-"+num+" ");
-                }while(i<y);
+                    if(num % 2 == 0){
+                        System.out.print("+" + num + " ");
+                    }
+                    else{
+                        System.out.print("-" + num + " ");
+                    }
+                } while(i < y);
                 System.out.println();
-            break;
-            case 3: Fibonacci f=new Fibonacci();
-                    f.serieFib();
-            break;
-            default: System.out.println("Seleccion incorrecta");
+                break;
+            case 3:
+                Fibonacci f=new Fibonacci();
+                f.serieFib();
+                break;
+            default:
+                System.out.println("Seleccion incorrecta");
         }
-        
+
     }
 }
